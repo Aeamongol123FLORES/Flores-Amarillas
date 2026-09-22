71fc409bf41d4024b9d3edc8eb3db71658bdf169
Aeamongol123FLORES <tomoekurama47@gmail.com>
2026-09-21 21:43:46 -0500

    Delete inicio.js

--- inicio.js
+++
@@ -1,8 +0,0 @@
-
-const boton = document.querySelector(".boton-entrar");
-
-if (boton) {
-    boton.addEventListener("click", () => {
-        console.log("Entrando al universo de flores amarillas 🌻");
-    });
-}
