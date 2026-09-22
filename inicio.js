28144873f8e673595b7bed5dbae6044f6407eeb6
Aeamongol123FLORES <tomoekurama47@gmail.com>
2026-09-21 19:15:17 -0500

    Add files via upload

---
+++ inicio.js
@@ -0,0 +1,8 @@
+
+const boton = document.querySelector(".boton-entrar");
+
+if (boton) {
+    boton.addEventListener("click", () => {
+        console.log("Entrando al universo de flores amarillas 🌻");
+    });
+}
